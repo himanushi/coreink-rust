5bacfb7957da216115b5c4ec03a4ccd870e2dc42
himanushi <himanushi@users.noreply.github.com>
2024-05-16 20:25:35 +0900

    fix

--- build.rs
+++ build.rs
@@ -5,13 +5,23 @@ fn main() {
     // ESP-IDFのパスを取得
     let idf_path = std::env::var("IDF_PATH").expect("IDF_PATH environment variable is not set");
 
+    // 標準ライブラリのインクルードパスを設定
+    let cpp_include_path1 = "/opt/xtensa-esp32-elf/xtensa-esp32-elf/include/c++/8.4.0";
+    let cpp_include_path2 = "/opt/xtensa-esp32-elf/xtensa-esp32-elf/include/c++/8.4.0/xtensa-esp32-elf";
+    let cpp_include_path3 = "/opt/xtensa-esp32-elf/lib/gcc/xtensa-esp32-elf/8.4.0/include";
+
     // xtensa-esp32-elf-g++コンパイラを使用してC++ライブラリのビルド
     cc::Build::new()
         .cpp(true) // C++をビルドするためにcpp(true)を指定
         .compiler("xtensa-esp32-elf-g++") // ESP-IDF用のコンパイラを指定
         .flag("-std=c++11") // 必要に応じてC++の標準を指定
+        .flag("-fno-exceptions") // 例外処理を無効化
+        .flag("-fno-rtti") // RTTIを無効化
         .include("c_libs/M5GFX/src")
         .include("c_libs/M5Unified/src")
+        .include(cpp_include_path1) // 標準ライブラリのインクルードパスを追加
+        .include(cpp_include_path2) // 標準ライブラリのインクルードパスを追加
+        .include(cpp_include_path3) // 標準ライブラリのインクルードパスを追加
         .file("c_libs/M5GFX/src/M5GFX.cpp")
         .file("c_libs/M5Unified/src/M5Unified.cpp")
         .compile("m5");
