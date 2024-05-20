4d4382467c0d6f1198a5a1649d8ae6b6c21d1aba
himanushi <himanushi@users.noreply.github.com>
2024-05-20 17:41:37 +0900

    fix

--- build.rs
+++ build.rs
@@ -1,6 +1,5 @@
 extern crate bindgen;
 
-use std::env;
 use std::path::PathBuf;
 
 fn main() {
@@ -10,15 +9,27 @@ fn main() {
 
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
-        .clang_arg("-I/path/to/clibs/M5GFX/src")
-        .clang_arg("-I/path/to/clibs/M5Unified/src")
-        .clang_arg("-I/path/to/your_project_name")
+        .clang_arg("-Iesp-idf/components/esp_rom/include")
+        .clang_arg("-Iesp-idf/components/esp_driver_spi/include")
+        .clang_arg("-Iesp-idf/components/esp_hw_support/include")
+        .clang_arg("-Iesp-idf/components/esp_common/include")
+        .clang_arg("-Iesp-idf/components/esp_system/include")
+        .clang_arg("-Iesp-idf/components/esp_system/include")
+        .clang_arg("-Iesp-idf/components/hal/include")
+        .clang_arg("-Iesp-idf/components/soc/esp32/include")
+        .clang_arg("-Iclibs/M5GFX/src")
+        .clang_arg("-Iclibs/M5Unified/src")
+        .clang_arg("-I.")
+        .clang_arg("-I/usr/lib/gcc/aarch64-linux-gnu/12/include")
+        .clang_arg("-I/usr/local/include")
+        .clang_arg("-I/usr/include/aarch64-linux-gnu")
+        .clang_arg("-I/usr/include")
         .clang_arg("-xc++")
         .clang_arg("-std=c++14")
         .generate()
         .expect("Unable to generate bindings");
 
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let out_path = PathBuf::from("src");
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
