8b21ec1d61265c055280857fd74dbcfbbec3980c
himanushi <himanushi@users.noreply.github.com>
2024-05-20 18:43:14 +0900

    fix

--- wrapper.h
+++ wrapper.h
@@ -1,4 +1,9 @@
 #define ESP_PLATFORM
 
+#define CONFIG_FREERTOS_NUMBER_OF_CORES 1
+#define CONFIG_FREERTOS_MAX_TASK_NAME_LEN 16
+#define CONFIG_FREERTOS_TASK_NOTIFICATION_ARRAY_ENTRIES 1
+
+#include <memory>
 #include "clibs/M5GFX/src/M5GFX.h"
 #include "clibs/M5Unified/src/M5Unified.h"
